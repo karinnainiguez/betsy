1a03082557cdb537724b47a1997764debb9adb42
Karinna Iniguez <karinnainiguez@gmail.com>
2018-04-19 16:00:42 -0700

    Redid join table and added yml entries

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :categories, join_table: :products_categories
   has_many :reviews
 end
