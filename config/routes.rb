876d3f3f7c36f0ea4687f97ed3f3b162321692dd
knockknockhusthere <angela.huxia@gmail.com>
2018-04-19 14:13:37 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :products
+
+  resources :categories
+
+  resources :orders
+
+  resources :reviews
+
+  resources :users
 end
