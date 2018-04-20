47aa517175dc73c12110cfe7a95ec96ee55923df
knockknockhusthere <angela.huxia@gmail.com>
2018-04-19 23:51:06 -0700

    validation testing and validations added for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :categories, join_table: :products_categories
   has_many :reviews
+
+  validates :name, presence: true, uniqueness: { scope: :user }
+  validates :price, presence: true, numericality: { greater_than: 0}
+  validates :stock, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
 end
