157b24260dda40de61e76f215f1aeae9d6f6f8f9
knockknockhusthere <angela.huxia@gmail.com>
2018-04-20 15:45:20 -0700

    created controller actions and index tests for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,12 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :products, join_table: :products_categories
 
-  validates :name, presence: true, uniqueness: true 
+  validates :name, presence: true, uniqueness: true
+
+  def self.select_with_products
+    Category.all.select do |cat|
+      cat.products.count > 0
+    end
+  end
+
 end
