9eb3a4f12bd103acd9059894dd6a07e9e696b125
Cara <cararbrennan@gmail.com>
2018-04-19 20:50:33 -0700

    Adding model tests to test-order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,10 @@
 class Order < ApplicationRecord
-
+  validates :name, presence: true
+  validates :state, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :ccnumber, presence: true, uniqueness: true
+  validates :ccexpiration, presence: true
+  validates :cvv, presence: true
+  validates :address, presence: true
+  validates :zipcode, presence: true
 end
