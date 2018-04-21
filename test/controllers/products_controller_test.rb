5ec5fdc433b845d16ed148e9b25eae66dddb4f58
Cara <cararbrennan@gmail.com>
2018-04-21 12:16:16 -0700

    Pulling down from local master

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -17,9 +17,44 @@ describe ProductsController do
   end
 
   describe 'new' do
-    it 'creates a product associated with user' do
-      
+    it 'responds with success' do
+      get new_product_path
+      must_respond_with :success
     end
+
+    it 'creates a product associated with user' do
+
+      product = Product.first
+
+      product.user_id.wont_equal nil
+
+    end
+
+  end
+
+  it 'can add a product' do
+    # Arrange
+    product_data = {
+      name: "test-name",
+      price: 11,
+      stock: 4,
+      product_status: nil,
+      user_id: User.first.id
+    }
+    old_product_count = Product.count
+
+    # Assumptions
+    Product.new(product_data).must_be :valid?
+
+    # Act
+    post products_path, params: { product: product_data }
+
+    #Assert
+    must_respond_with :redirect
+    must_redirect_to products_path
+
+    Product.count.must_equal old_product_count + 1
+    Product.last.id.must_equal product_data[:id]
   end
 
   describe 'retired' do
@@ -36,6 +71,11 @@ describe ProductsController do
       @product.product_status.must_equal 'retired'
 
     end
-  end
 
+    # it 'retires products associated with user when user is deleted' do
+    #   products = user.products
+    #   user.destroy
+    #   products.first.product_status.must_equal 'retired'
+    # end
+  end
 end
