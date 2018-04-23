a06fac4331cd9a9ce93b47a91983e927aef69b27
Dikla <dikla.aharoni@gmail.com>
2018-04-22 23:38:58 -0700

    Added tests for create method and for new method in review controller Added new.html file for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,19 @@
 class ReviewsController < ApplicationController
-
+  before_action :find_product, only: [:create, :new]
   def new
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    @review.product = find_product
+    @review.product = @product
 
       if @review.save
-        redirect_to product_path
+        flash[:success] = "Review added successfully"
+        redirect_to product_path(@product)
       else
-        render :new
+        flash.now[:failure] = "Please add ratind"
+        render :new, status: :bad_request
       end
   end
 
@@ -27,5 +29,6 @@ class ReviewsController < ApplicationController
     def find_product
       @product = Product.find_by(id: params[:product_id])
       head :not_found unless @product
+      return @product
     end
 end
