325099f09f7d5c6b82c0e8c52b39ca34ef41ba7f
knockknockhusthere <angela.huxia@gmail.com>
2018-04-25 14:38:13 -0700

    added an end

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,14 @@
 module ApplicationHelper
-    def average_rating
-      reviews = self.reviews
+  def average_rating
+    reviews = self.reviews
 
-      return 0 if reviews.count == 0
+    return 0 if reviews.count == 0
 
-      total = reviews.map do |rating|
-        rating.rating
-      end.sum.to_f
+    total = reviews.map do |rating|
+      rating.rating
+    end.sum.to_f
 
-      return total / reviews.count
+    return total / reviews.count
 
-    end
   end
 end
