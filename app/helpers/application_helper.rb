845a195cfff825c3f9ae45fdab6232d8b0136419
knockknockhusthere <angela.huxia@gmail.com>
2018-04-25 14:36:21 -0700

    moved average_rating method to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def average_rating
+      reviews = self.reviews
+
+      return 0 if reviews.count == 0
+
+      total = reviews.map do |rating|
+        rating.rating
+      end.sum.to_f
+
+      return total / reviews.count
+
+    end
+  end
 end
