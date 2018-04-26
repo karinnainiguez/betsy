9f20ad5f61de63e97a056724a4949f1c3b6081f2
Karinna Iniguez <karinnainiguez@gmail.com>
2018-04-26 09:21:41 -0700

    Add delivery_method column to order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424223846) do
+ActiveRecord::Schema.define(version: 20180426162024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20180424223846) do
     t.string "zipcode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "delivery_method"
   end
 
   create_table "products", force: :cascade do |t|
