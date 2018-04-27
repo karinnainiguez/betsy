5c4b862433b0b33a67b309fd2065d13905928336
Cara <cararbrennan@gmail.com>
2018-04-26 18:23:31 -0700

    test coverage for cartitems in cartitems controller

--- test/controllers/cartitems_controller_test.rb
+++ test/controllers/cartitems_controller_test.rb
@@ -35,26 +35,62 @@ describe CartitemsController do
       }
       old_cartitem_count = Cartitem.count
 
-      # Assumptions
-
-
 
       # Act
       post product_cartitems_path(id), params: { cartitem: cartitem_data }
       #
       # # Assert
-      # must_respond_with :redirect
-      # must_redirect_to books_path
+      must_respond_with :redirect
+      must_redirect_to cart_path
       #
-      # Book.count.must_equal old_book_count + 1
+      Cartitem.count.must_equal old_cartitem_count + 1
       # Book.last.title.must_equal book_data[:title]
     end
 
   end
+  describe 'update' do
+    it "updates a quantity in cartitem from cart page" do
+      a = Product.find_by(name: 'chew toy')
+      id = a.id
+      cartitem_data = {
+        quantity: 1,
+        product_id: id,
+        order_id: Order.first.id
+      }
+      cart =  Cartitem.create!(cartitem_data)
+
+      # Act
+      patch update_cartitem_path(cart), params: { cartitem: {quantity: 2}}
+      cart.reload
+      # Assert
+
+      must_respond_with :redirect
+      must_redirect_to cart_path
+      # #
+      cart.quantity.must_equal  2
+
+    end
+
+    it "updates quantity of product from product page, when product already exists in cart" do
+      a = Product.find_by(name: 'chew toy')
+     get cart_path
+
+      product_id = a.id
+
+      post product_cartitems_path(product_id), params: { cartitem: {quantity: 2}}
+
+      post product_cartitems_path(product_id), params: { cartitem: {quantity: 2}}
+       b = Cartitem.last
+
+      b.quantity.must_equal 4
+      #call get path so that a session is set and then update the quantity, first loggin in by creating a cart
+      #add 1 quantity to a cart exists with a quantity and then we add the same product again
+    end
+  end
 
   describe 'delete' do
 
-    it "something" do
+    it "deletes a cartitem" do
 
       order = Order.create!
       product = Product.create!(
@@ -76,25 +112,6 @@ describe CartitemsController do
 
       Cartitem.count.must_equal old_count - 1
     end
-    
-
-    # old_count = Cartitem.count
-    #
-    # id = Cartitem.first.id
-    #
-    #
-    # delete delete_cartitem_path(id)
-    #
-    # must_respond_with :redirect
-    #       must_redirect_to cartitems_path
-    #
-    #       Cartitems.count.must_equal old_count - 1
-
 
   end
-
 end
-
-#delete - destroy
-#update - patch
-#quantity
